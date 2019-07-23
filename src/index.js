75ac56044fd74013635dce04377f1081784e27ae
Student <example@example.com>
2019-07-23 10:55:29 -0700

    Created NavBar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import NavBar from './components/NavBar/NavBar';
+import Welcome from './components/Welcome/Welcome';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => {
+  return <h1>Home page</h1>;
+};
+
+const About = () => {
+  return <h1>About page</h1>;
+};
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <NavBar />
+      <Route exact path="/" component={Welcome} />
+      <Route path="/about" component={About} />
+      <Route path="/home" component={Home} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
